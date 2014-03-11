bcac443c6ddb6dc6f1e7837fe6fa820d7b040ddf
justinmakaila <justinmakaila@gmail.com>
2014-03-11 12:34:15 -0400

    Added UIImage+animatedGif pod

---
+++ Podfile
@@ -0,0 +1,1 @@
+pod 'UIImage+animatedGif'
---
+++ Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// UIImage+animatedGif
+#define COCOAPODS_POD_AVAILABLE_UIImage_animatedGif
+#define COCOAPODS_VERSION_MAJOR_UIImage_animatedGif 0
+#define COCOAPODS_VERSION_MINOR_UIImage_animatedGif 1
+#define COCOAPODS_VERSION_PATCH_UIImage_animatedGif 0
+
