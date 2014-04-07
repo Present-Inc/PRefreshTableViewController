d3d07331a8d6bbe6bc91b57a1c8a44e9b76b4fec
justinmakaila <justinmakaila@gmail.com>
2014-04-07 10:45:49 -0400

    Added -endRefreshingWithError: required method in PRefreshTableViewControllerDelegate

--- PRefreshTableViewController.podspec
+++ PRefreshTableViewController.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = "PRefreshTableViewController"
-  s.version      = "0.0.3"
+  s.version      = "0.0.4"
   s.summary      = "A UIViewController that manages table view state and custom refresh header/footer views"
   s.description  = <<-DESC
                     PTableViewController
@@ -15,7 +15,7 @@ Pod::Spec.new do |s|
   s.license      = { :type => 'MIT', :file => 'LICENSE' }
   s.author       = { "justinmakaila" => "justinmakaila@gmail.com" }
   s.platform     = :ios, '7.0'
-  s.source       = { :git => "http://www.bitbucket.org/presenttv/PRefreshTableViewController.git", :tag => "0.0.3" }
+  s.source       = { :git => "http://www.bitbucket.org/presenttv/PRefreshTableViewController.git", :tag => s.version.to_s }
   s.source_files = 'PRefreshTableViewController/Classes/*.{h,m}'
   s.resource     = 'PRefreshTableViewController/Classes/*.{gif,xib}'
   s.dependency 'UIImage+animatedGif'
--- PRefreshTableViewController/Classes/PRefreshTableViewController.h
+++ PRefreshTableViewController/Classes/PRefreshTableViewController.h
@@ -22,61 +22,16 @@ typedef NS_ENUM(NSInteger, PTableViewControllerState) {
 
 @class PRefreshTableViewController;
 
-@protocol PTableViewControllerProtocol <NSObject>
+@protocol PRefreshTableViewControllerProtocol <NSObject>
 @required
-/**
- *  Operation to be performed for a refresh/load more call.
- *  
- *  @discussion This is typically where requests to an API are made
- */
 - (void)refreshBlock;
 
-/**
- *  Indicates whether the network is reachable
- *
- *  @discussion This method allows for the reachability status 
- *  to be determined by the individual subclass. Standard procedure
- *  is to use a reachability manager (Apple's Reachability, or 
- *  AFNetworkingReachabilityManager) to decide what network is
- *  reachable.
- *
- *  @return YES if a network (WiFi, WWAN, etc) is reachable, else NO
- */
 - (BOOL)isNetworkReachable;
-
-/**
- *  Indicates whether a refresh action can happen.
- *
- *  @discussion This method should be utilized to check the
- *  network reachability status, if the server has more results,
- *  and whether or not the controller is already fetching results.
- *
- *  @return YES if a refresh should happen, else NO
- */
 - (BOOL)canBeginRefresh;
-
-/**
- *  Indicates if the table view is empty.
- *
- *  @discussion The table view may be considered empty if the
- *  items array is empty, however, there are instances while using
- *  multiple sections where you may not want the table view to appear
- *  in the initializing state, due to static content.
- *
- *  @return YES if the table view is considered empty, else NO
- */
 - (BOOL)isTableViewEmpty;
 
-/**
- *  Handles ending the refresh cycle with results
- *
- *  @discussion This is the alternative to calling -endRefreshing.
- *  It is the point where the results are added to items and the
- *  table view is updated to reflect these additions
- *
- *  @param results The results from a server or cache
- */
 - (void)endRefreshingWithResults:(NSArray*)results;
+- (void)endRefreshingWithError:(NSError *)error;
 
 @optional
 - (void)observeModel;
@@ -85,11 +40,11 @@ typedef NS_ENUM(NSInteger, PTableViewControllerState) {
 
 @end
 
-@interface PRefreshTableViewController : UIViewController <PTableViewControllerProtocol, PDataSourceDelegate> {
+@interface PRefreshTableViewController : UIViewController <PRefreshTableViewControllerProtocol, PDataSourceDelegate> {
     UITableViewCellSeparatorStyle separatorStyle;
 }
 
-@property (weak, nonatomic) id<PTableViewControllerProtocol> delegate;
+@property (weak, nonatomic) id<PRefreshTableViewControllerProtocol> delegate;
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (nonatomic) PTableViewControllerState tableViewState;
